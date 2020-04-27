e55e3abb601ab48e8d9ca7309c62bd2bd713c9fe
VadimAfonievich <0711aiova@gmail.com>
2020-04-27 12:37:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+var leftBorderWidht = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(4/0);
+console.log(null);
+console.log(second);
+console.log('second');
+
+let persone = {
+    name : 'Jone',
+    age : 25,
+    isMarried : false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp', 4, 5, 6, 23];
+
+console.log(arr[2]);
+
+// alert ('Hello, world!');
+
+// let answer = confirm('Do you need something?');
+// console.log(answer);
+
+let answer = prompt('Do you need something?');
+console.log(answer);
+*/
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+a = 5;
+console.log(a);
